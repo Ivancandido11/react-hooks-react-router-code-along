68821fc4e2b28c5b9e8948e003c11d92984ae1cb
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-19 11:10:16 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./Components/App"
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+)
